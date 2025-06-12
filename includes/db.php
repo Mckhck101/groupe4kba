e3ab5dc8475f2ceb76775091ab0552b90d249cb8
Mckhck101 <134056058+Mckhck101@users.noreply.github.com>
2025-06-12 09:08:10 +0100

    Update db.php
    Ajout de la bd distante

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'localhost';
+/*$host = 'localhost';
 $dbname = 'groupe4_kba';
 $user = 'root';
 $password = '';
@@ -10,5 +10,26 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die('Erreur de connexion : ' . $e->getMessage());
+}*/
+$host = 'db4free.net';
+$db   = 'groupe4_kba';
+$user = 'bobmac';
+$pass = 'Mack1234@07';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,  // Pour voir les erreurs
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    // echo "Connexion réussie !";
+} catch (\PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+    exit;
 }
 ?>
